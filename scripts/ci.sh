c3f568f59728e495745ad43ff253b8a4df262a5f
Max Lincoln <max@devopsy.com>
2014-09-23 13:13:16 -0400

    Script for CI

---
+++ scripts/ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+python setup.py install
+./bin/wadlcrawler http://api.rackspace.com/wadls/
+# The cloud_monitoring WADL has a few issues, its a WIP
+rm wadls/cloud_monitoring.wadl
+./bin/wadl2swagger wadls/*.wadl
+

6a86d804e6cac87cde83b195831066214f2135b4
Max Lincoln <max@devopsy.com>
2014-09-25 19:40:00 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import setup
 
 setup(name='wadl2swagger',
-      version='0.0.1',
+      version='0.0.2',
       description='Convert WADL to Swagger',
       author='Max Lincoln',
       author_email='mlincoln@thoughtworks.com',

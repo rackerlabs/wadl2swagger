eb1890258db7d9d585769c7e31842026848e31b3
Max Lincoln <max@devopsy.com>
2014-11-21 15:01:34 -0500

    Create YAML-equivalent JSON files during build

--- build_site.sh
+++ build_site.sh
@@ -2,6 +2,9 @@
 cp -r wadls site/source/
 cp -r swagger site/source
 pushd site
+rm -rf build/
+rm source/swagger/*.json
 bundle install
+bundle exec thor yaml2json
 bundle exec middleman build
 popd
